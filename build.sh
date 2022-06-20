75c90a25dbfc6277a050c82148e5202907449215
arcosx <arcosx@outlook.com>
2022-06-20 14:24:15 +0800

    update

--- build.sh
+++ build.sh
@@ -37,22 +37,29 @@ function bb::test(){
         rm -f ${dir}/$(bb::dummy_file_name)
         ${builder}::prepare ${dindimg}
         local desc="${i} of ${n}: ${builder} #1 (clean env)"
+
         INFO "${desc}: starting"
         local begin=$(date +%s.%N)
         ${builder}::build ${dir} ${dindimg}
         local end=$(date +%s.%N)
         local took=$(echo ${end}-${begin} | bc)
+        local size=${builder}::imageSize
         INFO "${desc}: done, took ${took} seconds"
-        echo "${builder}-1,${took},${begin},${end}" >> ${csv}
+        echo "${builder},1,${took},${size}" >> ${csv}
+
+
         desc="${i} of ${n}: ${builder} #2 (with dummy modification. cache can be potentially used.)"
         date > ${dir}/$(bb::dummy_file_name)
+
         INFO "${desc}: starting"
         local begin=$(date +%s.%N)
         ${builder}::build ${dir} ${dindimg}
         local end=$(date +%s.%N)
         local took=$(echo ${end}-${begin} | bc)
         INFO "${desc}: done, took ${took} seconds"
-        echo "${builder}-2,${took},${begin},${end}" >> ${csv}
+        echo "${builder},2,${took},${size}" >> ${csv}
+
+
         INFO "${i} of ${n}: ${builder}: pruning"
         rm -f ${dir}/$(bb::dummy_file_name)
         ${builder}::prune
@@ -99,7 +106,7 @@ function docker::build(){
     if [ "$dindimg" == "docker:18.09-dind" ]; then
         INFO "dind is docker 18.09"
         docker run -v ${dir}:/workspace -w /workspace --rm --link $(bb::container_name docker):docker -e DOCKER_HOST=tcp://docker:2375 ${dindimg} \
-        docker build -t foo -q . > /dev/null 2>&1
+        docker build -t docker-build -q . > /dev/null 2>&1
     else
         INFO "dind version is not 18.09"
         # docker logs --tail 30 $(bb::container_name docker)
@@ -109,10 +116,15 @@ function docker::build(){
                     -v docker-certs-client:/certs/client:ro \
                     -v ${dir}:/workspace -w /workspace \
                     ${dindimg} \
-                    docker build -t foo -q . > /dev/null 2>&1
+                    docker build -t docker-build -q . > /dev/null 2>&1
     fi
     INFO "docker build success"
 }
+
+function docker::imageSize() {
+    echo $(docker images docker-build --format "{{.Size}}")
+}
+
 function docker::prune(){
     docker rm -f $(bb::container_name docker)
     docker volume rm -f $(bb::volume_name docker)
@@ -131,7 +143,10 @@ function buildkit::prepare(){
     docker volume create $(bb::volume_name buildkit)
     docker run --privileged --name $(bb::container_name buildkit) -d -v $(bb::volume_name buildkit):/var/lib/buildkit -p 1234:1234 ${BUILDKIT_IMAGE} \
            --addr tcp://0.0.0.0:1234
+    sleep 15
+    INFO "prepare buildkit success"
 }
+
 function buildkit::build(){
     local dir="$1"
     docker run \
@@ -141,17 +156,22 @@ function buildkit::build(){
            -e BUILDKIT_HOST=tcp://buildkit:1234 \
            --entrypoint buildctl \
            ${BUILDKIT_IMAGE} \
-           build --frontend=dockerfile.v0 --local context=. --local dockerfile=. > /dev/null 2>&1
+           build --frontend=dockerfile.v0 --local context=. --local dockerfile=. --output type=docker,name=buildkit-build > buildkit-build.tar
 }
 function buildkit::prune(){
     docker rm -f $(bb::container_name buildkit)
     docker volume rm -f $(bb::volume_name buildkit)
 }
 
+function buildkit::imageSize(){
+    docker load -i buildkit-build.tar
+    echo $(docker images buildkit-build --format "{{.Size}}")
+}
+
 
 
 ### Main
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 5 ]; then
     echo "Usage: $0 DIR CSV N DINDIMG"
     exit 1
 fi
@@ -161,7 +181,11 @@ CSV="$2"
 N="$3"
 DINDIMG="$4"
 
-builders=(docker buildkit)
+# both
+# docker
+# buildkit
+BUILDER="$5"
+
 
 # NOW=$(ls ${DIR})
 # INFO "DEBUG NOW ${NOW}"
@@ -169,7 +193,13 @@ INFO "DEBUG DIR ${DIR}"
 INFO "DEBUG CSV ${CSV}"
 INFO "DEBUG N ${N}"
 INFO "DEBUG DINDIMG ${DINDIMG}"
+INFO "DEBUG SPECIFIC ${SPECIFIC}"
 
-for builder in ${builders[@]}; do
-    bb::test ${builder} ${DIR} ${CSV} ${N} ${DINDIMG}
-done
+if [ "$BUILDER" == "both" ]; then
+    INFO "Both docker and buildkit will be test"
+    bb::test docker ${DIR} ${CSV} ${N} ${DINDIMG}
+    bb::test buildkit ${DIR} ${CSV} ${N} ${DINDIMG}
+else
+    INFO "Only ${BUILDER} will be test"
+    bb::test ${BUILDER} ${DIR} ${CSV} ${N} ${DINDIMG}
+fi
